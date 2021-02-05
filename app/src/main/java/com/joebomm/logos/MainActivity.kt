d265177c081d5ba543a1b650499d5215e299e2db
JoeBomm <jrbomm@gmail.com>
2021-02-04 19:55:52 -0500

    Create main activity buttons

--- app/src/main/java/com/joebomm/logos/MainActivity.kt
+++ app/src/main/java/com/joebomm/logos/MainActivity.kt
@@ -2,10 +2,20 @@ package com.joebomm.logos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    /** Called when the user taps the Two's Compliment button */
+    fun openTwosCompliment(view: View) {}
+
+    /** Called when the user taps the True or False button */
+    fun openTrueOrFalse(view: View) {}
+
+    /** Called when the user taps the About button */
+    fun openAbout(view: View) {}
 }
