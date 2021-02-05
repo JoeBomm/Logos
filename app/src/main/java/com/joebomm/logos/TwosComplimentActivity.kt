0d9ca96093704fd252a69d55482a409263909090
JoeBomm <jrbomm@gmail.com>
2021-02-04 22:41:01 -0500

    Make functions to find one and twos compliments. Show twos compliment of random binary number for debugging

--- app/src/main/java/com/joebomm/logos/TwosComplimentActivity.kt
+++ app/src/main/java/com/joebomm/logos/TwosComplimentActivity.kt
@@ -1,6 +1,5 @@
 package com.joebomm.logos
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -16,7 +15,11 @@ class TwosComplimentActivity : AppCompatActivity() {
     fun updateBinary(view: View) {
         val randomNumber = Random.nextInt(1, 256)
         val textView = findViewById<TextView>(R.id.textViewBinary)
-        textView.text = toBinary(randomNumber)
+        val binaryNum = toBinary(randomNumber)
+        textView.text = binaryNum
+
+        val textViewDe = findViewById<TextView>(R.id.textViewDebug)
+        textViewDe.text = twosCompliment(binaryNum)
     }
 
     private fun toBinary(number: Int) : String {
@@ -33,5 +36,36 @@ class TwosComplimentActivity : AppCompatActivity() {
         }
 
         return result.reversed()
+    }
+
+    private fun onesCompliment(number: String) : String {
+        var num = number.toCharArray()
+        for(i in num.indices) {
+            if(num[i]=='0') {
+                num[i]='1'
+            } else {
+                num[i]='0'
+            }
+        }
+        return String(num)
+    }
+
+    private fun twosCompliment(number: String) : String {
+        val num = onesCompliment(number.reversed()).toCharArray()
+        var carry = false
+        var index = 0
+        do {
+            if (num[index]=='0') {
+                num[index]='1'
+                carry = false
+            } else {
+                num[index]='0'
+                carry = true
+            }
+            index++
+
+        } while (index<8 && carry)
+
+        return String(num).reversed()
     }
 }
